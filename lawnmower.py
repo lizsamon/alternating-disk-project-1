a6cadacc3ff64598b368d5cf2240451e4e3514b3
lizsamon <lizsamon@csu.fullerton.edu>
2024-10-07 17:06:02 -0700

    fixed lawnmower and lefttoright for loop logic

---
+++ lawnmower.py
@@ -0,0 +1,26 @@
+disk_list = ['dark', 'light', 'dark', 'light', 'dark', 'light','dark', 'light'] # initializing unsorted disk list as an array of strings
+
+def lawnmower(disk_list): # defining the lawnmower function and passing in the disk_list input
+
+    n = len(disk_list) // 2 # setting n as half the length of the disk_list since there are 2n disks in total
+
+    for i in range(n): # outer loop which runs n/2 times through the disk_list 
+
+        for j in range(0, len(disk_list) - 1): # left to right inner loop which iterates from the first index to the last index
+
+            if disk_list[j] == 'dark' and disk_list[j+1] == 'light': # checks if the disk is dark and its right adjacent disk is light
+
+                disk_list[j], disk_list[j+1] = disk_list[j+1], disk_list[j] # when condition met, swaps the disk with it's right adjacent disk
+
+        for j in range(len(disk_list) - 1, 0, -1): # right to left inner loop which iterates from the last index to the first index
+             
+             if disk_list[j] == 'light' and disk_list[j-1] == 'dark': # checks if the disk is light and its left adjacent disk is dark
+                 
+                 disk_list[j], disk_list[j-1] = disk_list[j-1], disk_list[j] #when condition is met, swaps the disk with it's left adjacent disk
+                        
+
+    return disk_list # returns the sorted disk_list
+
+diskListsorted = lawnmower(disk_list) # assigning lawnmower function to a variable
+
+print(diskListsorted) # prints the sorted disk_list, ex: ['light', 'light', 'light', 'light', 'dark', 'dark', 'dark', 'dark']
--- lefttoright.py
+++ lefttoright.py
@@ -2,19 +2,18 @@ disk_list = ['dark', 'light', 'dark', 'light', 'dark', 'light', 'dark', 'light']
 
 def leftToright(disk_list): # defining the left-to-right function and passing in the disk_list input
 
-    n = len(disk_list) // 2 # setting n as half the length of the disk_list since there are 2n disks in total
+    n = len(disk_list) # setting n as the length of the disk_list
 
-    for i in range(n): # outer loop which iterates n times through the disk_list
+    for i in range(n): # outer loop which runs n times through the disk_list
 
-        for j in range(1,2*n): # inner loop which iterates from index 1 to 2n-1 
+        for j in range(0, n - 1): # inner loop which iterates from index 0 to n - 1 
 
-            if disk_list[j] == 'light' and disk_list[j-1] == 'dark': # if a disk is 'light' and that disk's left adjacent disk is 'dark', swap 
+            if disk_list[j] == 'dark' and disk_list[j+1] == 'light': # checks if a disk is 'dark' and that disk's right adjacent disk is 'light'
                 
-                disk_list[j], disk_list[j-1] = disk_list[j-1], disk_list[j] # swaps the right disk with its adjacent left disk
+                disk_list[j], disk_list[j+1] = disk_list[j+1], disk_list[j] # when conditions met, swaps the disk with its adjacent right disk
 
     return disk_list # returns the sorted disk_list
 
-
 diskListsorted = leftToright(disk_list) # assigning leftToright function to a variable
 
-print(diskListsorted) # prints sorted disk_list: ['light', 'light', 'light', 'light', 'dark', 'dark', 'dark', 'dark']
+print(diskListsorted) # prints sorted disk_list, ex: ['light', 'light', 'light', 'light', 'dark', 'dark', 'dark', 'dark']
